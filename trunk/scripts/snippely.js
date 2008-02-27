f1e4da34a78b3aa5801615adb4d296c08f5e5384
tomocchino <tomocchino@94106105-ba46-0410-9688-1ba7413c6fa7>
2008-02-27 08:25:06 +0000

    - new layout (okay its still missing the pill buttons but im tired)

--- trunk/scripts/snippely.js
+++ trunk/scripts/snippely.js
@@ -10,69 +10,11 @@ Application.autoExit = true;
 
 var Snippely = {
 	
-	createMenus: function(){ //create the menus
-		
-		this.mainMenu = new ART.Menu('MainMenu');
-		
-		this.saveItem = new ART.Menu.Item('Save');
-		this.saveItem.shortcut = 'command+s';
-		
-		this.loadItem = new ART.Menu.Item('Load');
-		this.loadItem.shortcut = 'command+l';
-
-		this.fileMenu = new ART.Menu('File').addItem(this.saveItem).addItem(this.loadItem);
-		this.mainMenu.addMenu(this.fileMenu);
-		
-		//add menu
-		
-		this.addMenu = new ART.Menu('AddMenu');
-		
-		this.addTagItem = new ART.Menu.Item('Add Tag...');
-		this.addSnippetItem = new ART.Menu.Item('Add Snippet...');
-		
-		this.addMenu.addItem(
-			this.addTagItem
-		).addItem(
-			this.addSnippetItem
-		);
-		
-		//action menu
-		
-		this.actionMenu = new ART.Menu('ActionMenu');
-		
-		this.removeTagItem = new ART.Menu.Item('Remove Tag...');
-		this.renameTagItem = new ART.Menu.Item('Rename Tag...');
-		
-		this.removeSnippetItem = new ART.Menu.Item('Remove Snippet...');
-		this.renameSnippetItem = new ART.Menu.Item('Rename Snippet...');
-		
-		this.actionMenu.addItem(
-			this.renameTagItem
-		).addItem(
-			this.removeTagItem
-		).addItem(
-			this.renameSnippetItem
-		).addItem(
-			this.removeSnippetItem
-		);
-		
-	},
-	
-	focus: function(){
-		document.body.id = 'focus';
-	},
-	
-	blur: function(){
-		document.body.id = 'blur';
-	},
-	
-	domReady: function(){
-		
+	initialize: function(){
 		nativeWindow.addEventListener('activate', this.focus);
-		
 		nativeWindow.addEventListener('deactivate', this.blur);
 		
-		$$('#panel, #list, #meta').addEvent('mousedown', function(event){ //disables selection on the whole panel
+		$$('#sidebar, #snippets, #content').addEvent('mousedown', function(event){ //disables selection on the whole panel
 			event.preventDefault();
 		});
 		
@@ -90,58 +32,86 @@ var Snippely = {
 			this.removeClass('active');
 		});
 		
-		//drag that panel
-		
-		var adaptFooter = function(){
-			$('panel-footer').setStyle('width', $('panel').clientWidth);
-		};
-		
-		new Drag($('panel'), {
-			handle: $$('#splitter, #panel-resizer'),
-			modifiers: {x: 'width', y: null},
-			limit: {x: [100, 400]},
-			onDrag: adaptFooter
+		//drag instances
+		new Draggable([
+			{ subject: 'content', property: 'left' },
+			{ subject: 'sidebar', property: 'width' },
+			{ subject: 'snippets', property: 'left' }
+		], {
+			min: 150,
+			max: 300,
+			axis: 'x',
+			handle: 'sidebar-resizer'
 		});
 		
-		new Drag($('list'), {
-			handle: $('content-resizer'),
-			modifiers: {x: null, y: 'height'},
-			limit: {y: [38, function(){
-				return $('list').scrollHeight;
-			}]}
+		new Draggable([
+			{ subject: 'content', property: 'top' },
+			{ subject: 'snippets', property: 'height' }
+		], {
+			min: 150,
+			max: 300,
+			axis: 'y',
+			handle: 'snippets-resizer'
 		});
 		
-		adaptFooter();
-		
-		//end drag
-		
-		//select those tags
-		
-		var tags = $$('.tag'); 
-		
+		//selectable items
+		var tags = $$('#tags li');
 		tags.addEvent('mouseup', function(){
 			tags.removeClass('selected');
 			this.addClass('selected');
 		});
-		
-		var items = $$('.item'); 
-		
-		items.addEvent('click', function(){
+		var items = $$('#snippets-list li');
+		items.addEvent('mouseup', function(){
 			items.removeClass('selected');
 			this.addClass('selected');
 		});
 		
-		//end select
+		//zebra striping
+		$$('#snippets-list li:odd').addClass('odd');
 		
+		//meta buttons
 		var metaButtons = $$('#meta .button');
-		
 		metaButtons.addEvent('mousedown', function(){
 			this.addClass('active');
 		});
-		
 		document.addEvent('mouseup', function(){
 			metaButtons.removeClass('active');
 		});
+
+		this.activate(); //activates the window, and sets it visible.
+	},
+	
+	createMenus: function(){
+		//main menus
+		this.mainMenu = new ART.Menu('MainMenu');
+		this.saveItem = new ART.Menu.Item('Save');
+		this.loadItem = new ART.Menu.Item('Load');
+		this.saveItem.shortcut = 'command+s';
+		this.loadItem.shortcut = 'command+l';
+		this.fileMenu = new ART.Menu('File').addItem(this.saveItem).addItem(this.loadItem);
+		this.mainMenu.addMenu(this.fileMenu);
+		
+		//add menu
+		this.addMenu = new ART.Menu('AddMenu');
+		this.addTagItem = new ART.Menu.Item('Add Tag...');
+		this.addSnippetItem = new ART.Menu.Item('Add Snippet...');
+		this.addMenu.addItem(this.addTagItem).addItem(this.addSnippetItem);
+		
+		//action menu
+		this.actionMenu = new ART.Menu('ActionMenu');
+		this.removeTagItem = new ART.Menu.Item('Remove Tag...');
+		this.renameTagItem = new ART.Menu.Item('Rename Tag...');
+		this.removeSnippetItem = new ART.Menu.Item('Remove Snippet...');
+		this.renameSnippetItem = new ART.Menu.Item('Rename Snippet...');
+		this.actionMenu.addItem(this.renameTagItem).addItem(this.removeTagItem).addItem(this.renameSnippetItem).addItem(this.removeSnippetItem);
+	},
+	
+	focus: function(){
+		document.body.id = 'focus';
+	},
+	
+	blur: function(){
+		document.body.id = 'blur';
 	},
 	
 	activate: function(){
@@ -152,8 +122,4 @@ var Snippely = {
 };
 
 Snippely.createMenus();
-
-window.addEvent('load', function(){
-	Snippely.domReady(); //performs actions on elements.
-	Snippely.activate(); //activates the window, and sets it visible.
-});
+window.addEvent('load', Snippely.initialize.bind(Snippely));
