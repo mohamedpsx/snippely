78af95ec49b1b2441133fad7a6d8b7f6fc9343f7
kamicane <kamicane@94106105-ba46-0410-9688-1ba7413c6fa7>
2008-03-14 23:45:02 +0000

    - highlight, php

--- scripts/highlighter/brushes.javascript.js
+++ scripts/highlighter/brushes.javascript.js
@@ -8,17 +8,13 @@ Brushes.JavaScript = {
 	
 	'integer': (/\b(\d+)\b/gm),
 	
-	'boolean': ['true', 'false', 'null'],
+	'boolean': 'true false null',
 	
-	'special': ['this'],
+	'special': 'this',
 	
-	'keyword': [
-		'abstract', 'boolean', 'break', 'byte', 'case', 'catch', 'char', 'class', 'const', 'continue',
-		'debugger', 'default', 'delete', 'do', 'double', 'else', 'enum', 'export', 'extends',
-		'final', 'finally', 'float', 'for', 'function', 'goto', 'if', 'implements', 'import', 'in',
-		'instanceof', 'int', 'interface', 'long', 'native', 'new', 'package', 'private', 'protected',
-		'public', 'return', 'short', 'static', 'super', 'switch', 'synchronized', 'throw', 'throws',
-		'transient', 'try', 'typeof', 'var', 'void', 'volatile', 'while', 'with', 'prototype'
-	]
+	'keyword': "abstract boolean break byte case catch char class const continue debugger default delete do double \
+		else enum export extends final finally float for function goto if implements import in instanceof int interface \
+		long native new package private protected public return short static super switch synchronized throw throws transient \
+		try typeof var void volatile while with prototype"
 
 };
